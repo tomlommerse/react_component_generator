dc773a1ae36c3ecab342ba63d1bb83a86e97d273
tomlommerse <tomlommerse2001@gmail.com>
2023-09-28 00:50:14 +0200

    initial push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav/Nav.js'
 
 function App() {
   return (
+    
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,10 @@
+import './Nav.css';
+function Nav() {
+return (
+<nav className="Nav">
+    <a href="https://www.google.com">hi</a>
+</nav>
+);
+}
+
+export default Nav;
